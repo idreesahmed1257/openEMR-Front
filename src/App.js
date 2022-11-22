f998aae167fe7004fe50db3eaf64e90a192d56ed
Ahmed Bilal <ahmedbilal6880@gmail.com>
2022-11-22 19:23:01 +0500

    gg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 // import addPatients from '../src/Pages/patient/addPatients'
 import AddPateints from '../src/Pages/patient/AddPatients'
+import AddPateints2 from '../src/Pages/patient/AddPatients'
+
 import Login from './Pages/registration/Login'
 
 const App = () => {
   return (
     <>
       {/* <AddPateints /> */}
-      <Login />
+      <AddPateints2 />
+      {/* <Login /> */}
     </>
   )
 }
